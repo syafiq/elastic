d8f07399bc5c2751402999e636fb0c488318ad02
Syafiq Al Atiiq <syafiq.atiiq@gmail.com>
2025-04-01 16:39:07 +0200

    fix: use Error type from sev crate root

--- elastic-hal/src/clock.rs
+++ elastic-hal/src/clock.rs
@@ -1,6 +1,7 @@
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use thiserror::Error;
-use sev::firmware::host::{Firmware, Error as SevError};
+use sev::firmware::host::Firmware;
+use sev::Error as SevError;
 
 #[derive(Debug, Error)]
 pub enum ClockError {
