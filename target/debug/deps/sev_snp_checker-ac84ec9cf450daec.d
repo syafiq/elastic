b4c32cc3b38c98d702897fea44730f69035ee2b2
Ubuntu <ubuntu@ip-172-31-4-243.eu-west-1.compute.internal>
2025-04-03 12:36:57 +0000

    chore: remove old example-clock-app directory

--- target/debug/deps/sev_snp_checker-ac84ec9cf450daec.d
+++
@@ -1,6 +0,0 @@
-/home/syafiq/elastic/target/debug/deps/sev_snp_checker-ac84ec9cf450daec: src/main.rs src/clock.rs
-
-/home/syafiq/elastic/target/debug/deps/sev_snp_checker-ac84ec9cf450daec.d: src/main.rs src/clock.rs
-
-src/main.rs:
-src/clock.rs:
--- target/debug/sev-snp-checker.d
+++
@@ -1,1 +0,0 @@
-/home/syafiq/elastic/target/debug/sev-snp-checker: /home/syafiq/elastic/src/clock.rs /home/syafiq/elastic/src/main.rs
